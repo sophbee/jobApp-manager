aef8aec7839985f9b994c8bb7f0f6de9ab68f5f3
Sophia Butterworth <sophiabutterworth@gmail.com>
2019-04-23 15:31:19 -0400

    Adding build folder for gh-pages

---
+++ build/precache-manifest.88db4fbf44e3837175cc9bc9e94bcb36.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "fa98c7a1cdec238051f2b5fb84be27e2",
+    "url": "/jobApp-manager/static/media/snag-logo-white.fa98c7a1.svg"
+  },
+  {
+    "revision": "32efd6b2fab082dd5052",
+    "url": "/jobApp-manager/static/js/runtime~main.e5df884a.js"
+  },
+  {
+    "revision": "498281439d51b161df16",
+    "url": "/jobApp-manager/static/js/main.b3e0d066.chunk.js"
+  },
+  {
+    "revision": "343bb12876224ee0df50",
+    "url": "/jobApp-manager/static/js/2.0bd40ddb.chunk.js"
+  },
+  {
+    "revision": "498281439d51b161df16",
+    "url": "/jobApp-manager/static/css/main.7d08a241.chunk.css"
+  },
+  {
+    "revision": "dd3eda1ef4e24a22a0c49e98f7fce89e",
+    "url": "/jobApp-manager/index.html"
+  }
+];
